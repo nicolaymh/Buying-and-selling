01bf57e2e8f52e2f8ba24771d0f22184477a8af3
Nicolay Moreno <83971977+nicolaymh@users.noreply.github.com>
2021-06-14 21:04:57 -0500

    Update cars.js

--- app/information/cars.js
+++ app/information/cars.js
@@ -4,21 +4,21 @@ const cars = [
         model: "landcruiser",
         year:  2019,
         price: 90000,
-        image: "/img/car-motorcycle/car brand/toyora-landcruiser-2019.png"
+        image: "img/car-motorcycle/car brand/toyora-landcruiser-2019.png"
     },    
     {
         brand: "Audi",
         model: "A6",
         year:  2019,
         price: 40000,
-        image: "/img/car-motorcycle/car brand/audi-a6-2019.png"
+        image: "img/car-motorcycle/car brand/audi-a6-2019.png"
     },
     {
         brand: "Renault",
         model: "sandero",
         year:  2019,
         price: 20000,
-        image: "/img/car-motorcycle/car brand/reanult-sandero-2019.png"
+        image: "img/car-motorcycle/car brand/reanult-sandero-2019.png"
     },
     {
         brand: "Renault",
@@ -39,48 +39,48 @@ const cars = [
         model: "Q3",
         year:  2018,
         price: 30000,
-        image: "/img/car-motorcycle/car brand/audi-q3-2018.png"
+        image: "img/car-motorcycle/car brand/audi-q3-2018.png"
     },
     {
         brand: "Chevrolet",
         model: "sail",
         year:  2019,
         price: 20000,
-        image: "/img/car-motorcycle/car brand/chevrolet-sail-2019.png"
+        image: "img/car-motorcycle/car brand/chevrolet-sail-2019.png"
     },  
     {
         brand: "Renault",
         model: "logan",
         year:  2018,
         price: 10000,
-        image: "/img/car-motorcycle/car brand/renault-logan-2018.png"
+        image: "img/car-motorcycle/car brand/renault-logan-2018.png"
     },
     {
         brand: "Chevrolet",
         model: "aveo",
         year:  2018,
         price: 10000,
-        image: "/img/car-motorcycle/car brand/chevrolet-aveo-2018.png"
+        image: "img/car-motorcycle/car brand/chevrolet-aveo-2018.png"
     },
     {
         brand: "Toyota",
         model: "hilux",
         year:  2017,
         price: 50000,
-        image: "/img/car-motorcycle/car brand/toyota-hilux-2017.png"
+        image: "img/car-motorcycle/car brand/toyota-hilux-2017.png"
     },
     {
         brand: "Toyota",
         model: "prado",
         year:  2018,
         price: 70000,
-        image: "/img/car-motorcycle/car brand/toyota-prado-2018.png"
+        image: "img/car-motorcycle/car brand/toyota-prado-2018.png"
     },
     {
         brand: "Chevrolet",
         model: "tahoe",
         year:  2018,
         price: 60000,
-        image: "/img/car-motorcycle/car brand/chevrolet-tahoe-2018.png"
+        image: "img/car-motorcycle/car brand/chevrolet-tahoe-2018.png"
     },  
 ]
