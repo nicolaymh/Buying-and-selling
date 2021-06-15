b9381504530a775da7f8fe203fec142fd63d7fad
Nicolay Moreno <83971977+nicolaymh@users.noreply.github.com>
2021-06-14 21:18:37 -0500

    Update motorcycles.js

--- app/information/motorcycles.js
+++ app/information/motorcycles.js
@@ -4,118 +4,118 @@ const motorcycles = [
         model: "HAYATE 115",
         year:  2017,
         price: 10000,
-        image: "/img/car-motorcycle/motorcycle brand/suzuki-hayate115-2017.png"
+        image: "./img/car-motorcycle/motorcycle brand/suzuki-hayate115-2017.png"
     },    
     {
         brand: "Suzuki",
         model: "DR650",
         year:  2018,
         price: 20000,
-        image: "/img/car-motorcycle/motorcycle brand/suzuki-DR650-2018.png"
+        image: "./img/car-motorcycle/motorcycle brand/suzuki-DR650-2018.png"
     },
     {
         brand: "Suzuki",
         model: "SV650",
         year:  2020,
         price: 50000,
-        image: "/img/car-motorcycle/motorcycle brand/suzuki-sv650-2020.png"
+        image: "./img/car-motorcycle/motorcycle brand/suzuki-sv650-2020.png"
     },
     {
         brand: "Suzuki",
         model: "V-STROM-DL-1000XA",
         year:  2020,
         price: 100000,
-        image: "/img/car-motorcycle/motorcycle brand/Suzuki-V-STROM-DL-1000XA-2020.png"
+        image: "./img/car-motorcycle/motorcycle brand/Suzuki-V-STROM-DL-1000XA-2020.png"
     },
     {
         brand: "Suzuki",
         model: "GW250",
         year:  2019,
         price: 30000,
-        image: "/img/car-motorcycle/motorcycle brand/Suzuki-GW250-2019.png"
+        image: "./img/car-motorcycle/motorcycle brand/Suzuki-GW250-2019.png"
     },
     {
         brand: "Honda",
         model: "CBR250R",
         year:  2019,
         price: 90000,
-        image: "/img/car-motorcycle/motorcycle brand/honda-CBR250R-2019.png"
+        image: "./img/car-motorcycle/motorcycle brand/honda-CBR250R-2019.png"
     },
     {
         brand: "Honda",
         model: "GoldWing",
         year:  2020,
         price: 70000,
-        image: "/img/car-motorcycle/motorcycle brand/honda-GoldWing-2020.png"
+        image: "./img/car-motorcycle/motorcycle brand/honda-GoldWing-2020.png"
     },
     {
         brand: "Honda",
         model: "Rebel",
         year:  2017,
         price: 30000,
-        image: "/img/car-motorcycle/motorcycle brand/Honda-Rebel-2017.png"
+        image: "./img/car-motorcycle/motorcycle brand/Honda-Rebel-2017.png"
     },
     {
         brand: "Honda",
         model: "Metropolitan",
         year:  2018,
         price: 50000,
-        image: "/img/car-motorcycle/motorcycle brand/Honda-Metropolitan-2018.png"
+        image: "./img/car-motorcycle/motorcycle brand/Honda-Metropolitan-2018.png"
     },
     {
         brand: "Honda",
         model: "CRF50F",
         year:  2020,
         price: 80000,
-        image: "/img/car-motorcycle/motorcycle brand/Honda-CRF50F-2020.png"
+        image: "./img/car-motorcycle/motorcycle brand/Honda-CRF50F-2020.png"
     },    
     {
         brand: "Yamaha",
         model: "BWS-X-FI",
         year:  2018,
         price: 40000,
-        image: "/img/car-motorcycle/motorcycle brand/yamaha-bws-x-fi-2018.png"
+        image: "./img/car-motorcycle/motorcycle brand/yamaha-bws-x-fi-2018.png"
     },
     {
         brand: "Yamaha",
         model: "YCZ",
         year:  2017,
         price: 20000,
-        image: "/img/car-motorcycle/motorcycle brand/yamaha-YCZ-2017.png"
+        image: "./img/car-motorcycle/motorcycle brand/yamaha-YCZ-2017.png"
     },
     {
         brand: "Yamaha",
         model: "R6",
         year:  2020,
         price: 60000,
-        image: "/img/car-motorcycle/motorcycle brand/yamaha-R6-2020.png"
+        image: "./img/car-motorcycle/motorcycle brand/yamaha-R6-2020.png"
     },
     {
         brand: "Yamaha",
         model: "YZ85LW",
         year:  2019,
         price: 70000,
-        image: "/img/car-motorcycle/motorcycle brand/yamaha-YZ85-2019.png"
+        image: "./img/car-motorcycle/motorcycle brand/yamaha-YZ85-2019.png"
     },
     {
         brand: "Auteco",
         model: "Pulsar NS150",
         year:  2017,
         price: 10000,
-        image: "/img/car-motorcycle/motorcycle brand/AUTECO-PULSAR-NS150-2017.png"
+        image: "./img/car-motorcycle/motorcycle brand/AUTECO-PULSAR-NS150-2017.png"
     },
     {
         brand: "Auteco",
         model: "KTM Duke",
         year:  2018,
         price: 30000,
-        image: "/img/car-motorcycle/motorcycle brand/auteco-ktm-duke-200-2018.png"
+        image: "./img/car-motorcycle/motorcycle brand/auteco-ktm-duke-200-2018.png"
     },
     {
         brand: "Auteco",
         model: "Super Duke R",
         year:  2020,
         price: 100000,
-        image: "/img/car-motorcycle/motorcycle brand/auteco-super-duke-r-2020.png"
+        image: "./img/car-motorcycle/motorcycle brand/auteco-super-duke-r-2020.png"
     },
 ]
